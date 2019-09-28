fc213f45c0c99346c530a3c8e1ce1c1a1f3698b5
Hasaan <hasaansoomro@gmail.com>
2019-09-28 17:49:26 -0400

    server added

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    data = request.get_json()
+    #prediction = np.array2string(model.predict(data))
+    #return jsonify(prediction)
+    print(data)
+    return data
+
+if __name__ == '__main__':
+    #modelfile = 'models/final_prediction.pickle'
+    #model = p.load(open(modelfile, 'rb'))
+    app.run("localhost", "1111", debug=True)
